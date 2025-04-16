ff5552f26f49d61c4e6507ff0c01f4b74a463d8e
akshayvakil <akshayvakil1@gmail.com>
2025-04-16 23:23:50 +0530

    Changed key

--- PythonProject/agentAI1.py
+++ PythonProject/agentAI1.py
@@ -7,7 +7,7 @@ import asyncio
 
 
 async def Sitevalidation():
-    os.environ["GEMIMI_API_KEY"]= "AIzaSyCrM2P1Mm5e95rpzGXDWTGlLCZR3BPVQUs"
+    os.environ["GEMIMI_API_KEY"]= "AIzaSyCrM2P1Mm5e95rpzGXDWTGlLCZR3BPVQU" # add s
     print("Starting AI Agent code")
     task = (
         'I am automation tester validating a task '
